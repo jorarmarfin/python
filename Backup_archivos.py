0493eb395fab5583565b8fca8205171a2fcf8d8f
Luis mayta <luis.mayta@gmail.com>
2019-01-30 12:00:01 -0500

    Programa para hacer backup de base de datos

---
+++ Backup_archivos.py
@@ -0,0 +1,30 @@
+import subprocess,time, os
+
+fecha=time.strftime("%Y%m%d-%I%M")
+archivo = fecha+'.sql'
+database='october'
+user='root'
+password='root'
+archivo=database+'-'+archivo
+dump='mysqldump -u'+user+' -p'+password+' '+database+'>'+archivo
+os.system(dump)
+
+#Comprimir
+#usando libreria subprocess
+subprocess.call(['gzip',archivo])
+
+#Limpia Backup
+year=time.strftime("%Y")
+month=time.strftime("%m")
+if month=='12':
+    last_month=12
+    year= int(year)-1
+    year= str(year)
+else:
+    last_month= int(month)-1
+
+n="{:02d}".format(last_month)
+m= str(n)
+p=database+'-'+year+m+'*'
+
+os.system('rm -f '+p)
