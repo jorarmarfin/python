2ecba90ba8ce45af30acfdf844d48a2c532f97a1
Luis mayta <luis.mayta@gmail.com>
2019-06-04 10:42:49 -0500

    ultimso cambios

--- Elasticsearch.py
+++ Elasticsearch.py
@@ -1,5 +1,20 @@
-import requests
-res = requests.get('http://94.130.73.248:9200')
-
+import requests,json
 from elasticsearch import Elasticsearch
+from datetime import datetime
+
+#Prueba conexion
+#res = requests.get('http://94.130.73.248:9200')
+
 es = Elasticsearch([{'host': '94.130.73.248', 'port': 9200}])
+
+def creaindex():
+    r = requests.get('http://94.130.73.248:9200') 
+    i = 1
+    while r.status_code == 200:
+        r = requests.get('http://swapi.co/api/people/'+ str(i))
+        es.index(index='sw', doc_type='people', id=i, body=json.loads(r.content))
+        i=i+1
+    
+    print(i)
+
+creaindex()
