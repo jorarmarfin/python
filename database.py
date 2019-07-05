93b62632c133bd6104ba43038c5a8c37505ddfdc
Luis Mayta <luis.mayta@gmail.com>
2019-07-05 01:13:50 -0500

    Conexion Db

---
+++ database.py
@@ -0,0 +1,14 @@
+import MySQLdb
+v_host = 'localhost'
+v_user = 'root'
+v_db = 'emanuel'
+v_pass = 'P@ssw0rd'
+db = MySQLdb.connect(host=v_host, user=v_user,passwd=v_pass, db=v_db)
+cursor = db.cursor()
+#Variable
+recs=cursor.execute("SELECT * FROM movimiento limit 1")
+for x in range(recs):
+   print(cursor.fetchone())
+#Indirectamente
+for row in cursor:
+   print(row)
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
-<h1>Aprendiendo Python</h1>
+# Aprendiendo Python
+* apt-get install python3-pip
+
+## DataBase
+* apt-get install python-pip python-dev libmysqlclient-dev
+* pip3 install mysqlclient
