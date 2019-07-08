5b1275e80745fc0d9c99a74f20ad398a44f5588f
Luis mayta <luis.mayta@gmail.com>
2019-07-08 15:57:55 -0500

    nuevo paquete de conexio para mysql

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
-import MySQLdb
+import pymysql
 v_host = 'localhost'
 v_user = 'root'
 v_db = 'emanuel'
 v_pass = 'P@ssw0rd'
-db = MySQLdb.connect(host=v_host, user=v_user,passwd=v_pass, db=v_db)
+db = pymysql.connect(host=v_host, user=v_user,passwd=v_pass, db=v_db)
 cursor = db.cursor()
 #Variable
 recs=cursor.execute("SELECT * FROM movimiento limit 1")
